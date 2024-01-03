8e4b088c280218386c7930eb47be72a468c06396
obaydul_Kuasha <obaydul00000@gmail.com>
2024-01-03 21:37:48 +0600

    compilite file

--- src/Component/Page/Home/Home.jsx
+++ src/Component/Page/Home/Home.jsx
@@ -77,7 +77,7 @@ const Home = () => {
             {/* Add banner CArusall part End */}
             {/* Add Menu of Week part Stard */}
             <div className="py-10">
-                <div className="text-center text-3xl text-neutral-300 font-bold py-10">Menu of Week</div>
+                <div className="text-center text-3xl text-neutral-300 font-bold py-10 bg-[url(https://i.ibb.co/Bc6HCMH/section-bg.jpg)]">Menu of Week</div>
                 <div className="flex gap-8">
                     <div className="card card-side bg-slate-200 p-4 shadow-xl ">
                         <figure><img className="w-fit rounded-lg" src="https://i.ibb.co/2gXxGYW/menu-01.jpg " alt="Movie" /></figure>
@@ -129,7 +129,7 @@ const Home = () => {
             {/* Add Our Personal part Stard */}
 
             <div className="py-10">
-                <div className="text-center text-3xl text-neutral-300 font-bold py-10">Menu of Week</div>
+                <div className="text-center text-3xl text-neutral-300 font-bold py-10 bg-[url(https://i.ibb.co/Bc6HCMH/section-bg.jpg)]">Menu of Week</div>
                 <div className="flex gap-8">
                     <div className="card card-side bg-slate-200 p-4 shadow-xl ">
                         <figure><img className=" rounded-full" src="https://i.ibb.co/8P7B9kc/personal-01.jpg " alt="Movie" /></figure>
@@ -174,6 +174,237 @@ const Home = () => {
 
             </div>
             {/* Add Our Personal part End */}
+            {/* Add Our Why we ? part Stard */}
+            <div className="py-10">
+                <div className="text-center text-3xl text-neutral-300 font-bold py-10 bg-[url(https://i.ibb.co/Bc6HCMH/section-bg.jpg)]">Why We?</div>
+                <div>
+                    <ul className="steps steps-vertical lg:steps-horizontal">
+                        <li className="step step-primary">
+                            <h1 className="text-3xl font-bold">Best Coffee </h1>
+                            <p className="w-3/5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, laudantium.</p>
+                        </li>
+                        <li className="step step-warning">
+                            <h1 className="text-3xl font-bold">Fast service </h1>
+                            <p className="w-3/5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, laudantium.</p>
+                        </li>
+                        <li className="step step-warning">
+                            <h1 className="text-3xl font-bold">Good Menu </h1>
+                            <p className="w-3/5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, laudantium.</p>
+                        </li>
+                        <li className="step">
+                            <h1 className="text-3xl font-bold"> Just good </h1>
+                            <p className="w-3/5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis, laudantium.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            {/* Add Our Why we ? part End */}
+            {/* Add Our Next Event part Stard */}
+            <div className="py-10">
+                <div className="text-center text-3xl text-neutral-300 font-bold py-10 bg-[url(https://i.ibb.co/Bc6HCMH/section-bg.jpg)]">Why We?</div>
+                <div>
+                    <div className="card card-side bg-base-100 shadow-xl">
+                        <figure><img className="" src="https://i.ibb.co/3mjCwQf/book.png" alt="Movie" /></figure>
+                        <div className="card-body w-3/4">
+                            <h2 className="card-title text-4xl font-bold">Marry Christmas</h2>
+                            <p className="text-2xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam deserunt omnis enim possimus quae odit suscipit tempore qui voluptatum, animi sequi debitis necessitatibus sint eos error amet maxime modi molestias!</p>
+                            <div className="card-actions ">
+                                <button className="btn btn-outline">GO</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Add Our Next Event part End */}
+            {/* Add Our Last From Blog part Stard */}
+            <div className="py-10">
+                <div className="text-center text-3xl text-neutral-300 font-bold py-10 bg-[url(https://i.ibb.co/Bc6HCMH/section-bg.jpg)]">Last from blog</div>
+                <div className="flex flex-col gap-8">
+                    <div className="card card-side bg-base-100 shadow-xl">
+                        <figure><img className="w-96" src="https://i.ibb.co/jZyNwGF/blog-01.png" alt="Movie" /></figure>
+                        <div className="card-body w-3/4">
+                            <h2 className="card-title text-4xl font-bold">Cookies</h2>
+                            <div>
+                                <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 15 }}></span>
+                                        </span>
+                                        days
+                                    </div>
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 10 }}></span>
+                                        </span>
+                                        hours
+                                    </div>
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 24 }}></span>
+                                        </span>
+                                        min
+                                    </div>
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 30 }}></span>
+                                        </span>
+                                        sec
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="text-2xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam deserunt omnis enim possimus quae odit suscipit tempore qui voluptatum, animi sequi debitis necessitatibus sint eos error amet maxime modi molestias!</p>
+                            <div className="card-actions ">
+                                <button className="btn btn-outline">GO</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card card-side bg-base-100 shadow-xl">
+                        <figure><img className="w-96 " src="https://i.ibb.co/cws7gQz/blog-02.jpg" alt="Movie" /></figure>
+                        <div className="card-body w-3/4">
+                            <h2 className="card-title text-4xl font-bold">Broccoli alad</h2>
+                            <div>
+                                <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 15 }}></span>
+                                        </span>
+                                        days
+                                    </div>
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 10 }}></span>
+                                        </span>
+                                        hours
+                                    </div>
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 24 }}></span>
+                                        </span>
+                                        min
+                                    </div>
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 30 }}></span>
+                                        </span>
+                                        sec
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="text-2xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam deserunt omnis enim possimus quae odit suscipit tempore qui voluptatum, animi sequi debitis necessitatibus sint eos error amet maxime modi molestias!</p>
+                            <div className="card-actions ">
+                                <button className="btn btn-outline">GO</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card card-side bg-base-100 shadow-xl">
+                        <figure><img className="w-96" src="https://i.ibb.co/PZjKf7r/blog-03.jpg" alt="Movie" /></figure>
+                        <div className="card-body w-3/4">
+                            <h2 className="card-title text-4xl font-bold">Cookies</h2>
+                            <div>
+                                <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 15 }}></span>
+                                        </span>
+                                        days
+                                    </div>
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 10 }}></span>
+                                        </span>
+                                        hours
+                                    </div>
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 24 }}></span>
+                                        </span>
+                                        min
+                                    </div>
+                                    <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content">
+                                        <span className="countdown font-mono text-5xl">
+                                            <span style={{ "--value": 30 }}></span>
+                                        </span>
+                                        sec
+                                    </div>
+                                </div>
+                            </div>
+                            <p className="text-2xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam deserunt omnis enim possimus quae odit suscipit tempore qui voluptatum, animi sequi debitis necessitatibus sint eos error amet maxime modi molestias!</p>
+                            <div className="card-actions ">
+                                <button className="btn btn-outline">GO</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Add Our Last From Blog part End */}
+            {/* Add Our Gallery part stard */}
+            <div className="py-10">
+                <div className="text-center text-3xl text-neutral-300 font-bold py-10 bg-[url(https://i.ibb.co/Bc6HCMH/section-bg.jpg)]">Gallery</div>
+                <div>
+                    <div className="carousel rounded-box">
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Add Our Gallery part End */}
+            {/* Add Our Footer part Stard */}
+            <div className="py-10">
+                <footer className="footer p-10 bg-base-200 text-base-content">
+                    <aside>
+                        <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
+                        <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                    </aside>
+                    <nav>
+                        <header className="footer-title">Services</header>
+                        <a className="link link-hover">Branding</a>
+                        <a className="link link-hover">Design</a>
+                        <a className="link link-hover">Marketing</a>
+                        <a className="link link-hover">Advertisement</a>
+                    </nav>
+                    <nav>
+                        <header className="footer-title">Company</header>
+                        <a className="link link-hover">About us</a>
+                        <a className="link link-hover">Contact</a>
+                        <a className="link link-hover">Jobs</a>
+                        <a className="link link-hover">Press kit</a>
+                    </nav>
+                    <nav>
+                        <header className="footer-title">Legal</header>
+                        <a className="link link-hover">Terms of use</a>
+                        <a className="link link-hover">Privacy policy</a>
+                        <a className="link link-hover">Cookie policy</a>
+                    </nav>
+                </footer>
+
+            </div>
+            {/* Add Our Footer part End */}
+
+
 
 
 
